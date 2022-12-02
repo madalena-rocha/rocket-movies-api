37b854ba27df2980b98dc8abcfa6a58f5a242bc6
Madalena Machado Rocha <rochamada1997m@gmail.com>
2022-12-02 14:46:41 -0300

    add image upload

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 require("express-async-errors");
 const migrationsRun = require("./database/sqlite/migrations");
 const AppError = require("./utils/AppError");
+const uploadConfig = require("./configs/upload");
 
 const express = require("express");
 const routes = require("./routes");
@@ -9,6 +10,8 @@ migrationsRun();
 
 const app = express();
 app.use(express.json());
+
+app.use("/files", express.static(uploadConfig.UPLOADS_FOLDER));
 
 app.use(routes);
 
